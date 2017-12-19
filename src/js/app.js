1b7c61ebcf48fa9ca9ad41456ea7c64b846d4c54
purechild <a01056490114@gmail.com>
2017-12-19 14:21:37 +0900

    기본 기능 구현

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
 console.log('my-note app.js');
+
+window.onload = function (ev) {
+  text = document.getElementById('memo').value = localStorage.getItem('text');
+};
+
+btnNewnote = document.querySelector('.btn-newnote');
+btnNewnote.addEventListener('click', newNote);
+
+btnSavenote = document.querySelector('.btn-savenote');
+btnSavenote.addEventListener('click', saveNote);
+
+btnDownload = document.querySelector('.btn-notedownload');
+btnDownload.addEventListener('click', downloadNote);
+
+btnFullscr = document.querySelector('.btn-fullscreen');
+btnFullscr.addEventListener('click', scrFull);
+
+btnAbout = document.querySelector('.btn-about');
+btnAbout.addEventListener('click', showDecription);
+
+btnClose = document.getElementById('close_about');
+btnClose.addEventListener('click', hideDecription);
---
+++ src/js/util.js
@@ -0,0 +1,43 @@
+//새 노트
+var newNote = function () {
+  // console.log('새 노트 버튼');
+  document.getElementById('memo').value = '';
+};
+
+//노트 저장
+var saveNote = function() {
+  // console.log('저장 버튼');
+  localStorage.text = document.getElementById('memo').value;
+};
+
+//노트 다운로드
+var downloadNote = function () {
+  // console.log('다운로드 버튼');
+  var blob = new Blob([document.getElementById('memo').value], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, "text.txt");
+};
+
+//전체 화면
+var scrFull = function() {
+  if(screenfull.enabled){
+    // console.log('be fullscreened');
+    screenfull.request();
+  }
+  else{
+    alert("can\' be fullscreen");
+  }
+};
+
+//about
+about_layer = document.querySelector('.about_app');
+
+var showDecription = function () {
+  // console.log('about버튼');
+  about_layer.style.height = "auto";
+  about_layer.style.border = "1px black dashed";
+};
+
+var hideDecription = function() {
+  about_layer.style.height = "0";
+  about_layer.style.border = "none";
+};
